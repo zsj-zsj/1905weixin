57c8b9b21fdee29c1aeeae7e8932b697189c17f4
zsj-zsj <2211045409@qq.com>
2019-12-12 13:06:34 +0800

    回复

--- app/Http/Controllers/Weixin/Wxcontroller.php
+++ app/Http/Controllers/Weixin/Wxcontroller.php
@@ -1,29 +1,21 @@
 <?php
-
 namespace App\Http\Controllers\Weixin;
-
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Model\WxUserModel;
-
 class Wxcontroller extends Controller
 {
     protected $access_token;
-
     public function __construct(){
         //获取access_token
         $this->access_token=$this->getAccessToken();
-
     }
-
     protected function getAccessToken(){
         $url='https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid='.env('WX_APPID').'&secret='.env('WX_APPSECRET').'';
         $data_json=file_get_contents($url);
         $arr=json_decode($data_json,true);
         return $arr['access_token'];
     }
-
-
     //处理连接
     public function checkSignature(){
         $token= 'zsj1234zsj1234zsj432';
@@ -41,7 +33,6 @@ class Wxcontroller extends Controller
                 die("not ok");
         }
     }
-
     //接受微信推送事件
     public function receiv(){
         //将接受的数据记录到日志文件
@@ -49,15 +40,13 @@ class Wxcontroller extends Controller
         $xml_str = file_get_contents("php://input");
         $data = date('Y-m-d H:i:s') . $xml_str;
         file_put_contents($log_file,$data,FILE_APPEND);
-
         $xml_obj = simplexml_load_string($xml_str);  //处理xml数据
-
         //判断消息类型
+
         $msg_type=$xml_obj->MsgType;
         $touser=$xml_obj->FromUserName;     //接受消息用户openid
         $fromuser=$xml_obj->ToUserName;     //开发者公众号id
         $time=time();
-
 
         $event=$xml_obj->Event;   //获取事件类型
         if($event=='subscribe'){
@@ -65,11 +54,29 @@ class Wxcontroller extends Controller
             $wxuser='https://api.weixin.qq.com/cgi-bin/user/info?access_token='.$this->access_token.'&openid='.$openid.'&lang=zh_CN';
             $wx_user=file_get_contents($wxuser);
             $WXUser=json_decode($wx_user,true);
-
             $u=WxUserModel::where(['openid'=>$openid])->first();
             if($u){
-                echo "欢迎回来";die;
+                //第二次关注
+                $name='欢迎回来'.$WXUser['nickname'];
+                $data=[
+                    'sex'=>$WXUser['sex'],
+                    'openid'=>$openid,
+                    'sub_time'=>$xml_obj->CreateTime,
+                    'nickname'=>$WXUser['nickname']
+                ];
+                WxUserModel::where('openid','=',$openid)->update($data);
+                $guanzhu='<xml>
+                <ToUserName><![CDATA['.$touser.']]></ToUserName>
+                <FromUserName><![CDATA['.$fromuser.']]></FromUserName>
+                <CreateTime>'.$time.'</CreateTime>
+                <MsgType><![CDATA[text]]></MsgType>
+                <Image>
+                  <MediaId><![CDATA['.$name.']]></MediaId>
+                </Image>
+              </xml>';
+                echo $guanzhu;
             }else{
+                //第一次关注
                 $name='感谢您的关注'.$WXUser['nickname'];
                 $user_data=[
                     'sex'=>$WXUser['sex'],
@@ -78,7 +85,7 @@ class Wxcontroller extends Controller
                     'nickname'=>$WXUser['nickname']
                 ];
     
-                $uid =WxUserModel::insertGetId($user_data);
+                WxUserModel::insertGetId($user_data);
                 $guanzhu='<xml>
                 <ToUserName><![CDATA['.$touser.']]></ToUserName>
                 <FromUserName><![CDATA['.$fromuser.']]></FromUserName>
@@ -91,14 +98,11 @@ class Wxcontroller extends Controller
                 echo $guanzhu;
             }
             
-
             $url='https://api.weixin.qq.com/cgi-bin/user/info?access_token='.$this->access_token.'&openid='.$openid.'&lang=zh_CN';
             $user_info=file_get_contents($url);
             file_put_contents('wx_user.log',$user_info,FILE_APPEND);  
         }
-
         
-
         //文字
         if($msg_type=='text'){
             $content = date('Y-m-d H:i:s') . $xml_obj->Content;
@@ -140,9 +144,6 @@ class Wxcontroller extends Controller
           echo $Med;
         }
     }
-
-
-
     //获取用户基本信息
     public function  getUserInfo($access_token,$openid){
         $url ="https://api.weixin.qq.com/cgi-bin/user/info?access_token='.$access_token.'&openid='.$openid.'&lang=zh_CN";
