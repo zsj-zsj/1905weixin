30890b95907bfec6cef3b1ec468797ceb5131cf7
zsj-zsj <2211045409@qq.com>
2019-12-18 15:36:33 +0800

    shuliang

--- app/Http/Controllers/Weixin/VoteController.php
+++ app/Http/Controllers/Weixin/VoteController.php
@@ -4,11 +4,15 @@ namespace App\Http\Controllers\Weixin;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Redis;
 
 class VoteController extends Controller
 {
     public function index(){
         $data=$_GET;
+        if(empty($data)){
+            return "想干嘛?";
+        }
         $code=$data['code'];
 
         //获取access_token
@@ -17,7 +21,11 @@ class VoteController extends Controller
         $access_token=$token['access_token'];
         $openid=$token['openid'];
         $xinxi=$this->user($access_token,$openid);
-        dd($xinxi);
+        
+        $redis_key='vote';
+        $number=Redis::incr($redis_key);
+        echo "投票成功，当前票数：5869656".$number;
+
     }
     
     //获取code
