f19d1b75d0bdaaa5df6346dd1fbb14c66b5f74cb
zsj-zsj <2211045409@qq.com>
2019-12-18 12:43:55 +0800

    e

--- app/Http/Controllers/Weixin/Wxcontroller.php
+++ app/Http/Controllers/Weixin/Wxcontroller.php
@@ -250,7 +250,7 @@ class Wxcontroller extends Controller
     //菜单
     public function caidan(){
         $url="https://api.weixin.qq.com/cgi-bin/menu/create?access_token=".$this->access_token;
-        $urlEncode=urlEncode('http://1905zhangshaojie.comcto.com/vote');
+        $urlEncode=urlencode('http://1905zhangshaojie.comcto.com/vote');
         $menu=[
             'button' => [
               [
