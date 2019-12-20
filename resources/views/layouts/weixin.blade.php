3ab5289fcca577429dae241f83d6f34084c7c1bc
zsj-zsj <2211045409@qq.com>
2019-12-20 12:02:16 +0800

    f

--- resources/views/layouts/weixin.blade.php
+++ resources/views/layouts/weixin.blade.php
@@ -21,7 +21,36 @@
 	<link rel="shortcut icon" href="/img/favicon.png">
 
 </head>
-<body>    
+<body>  
+		<!-- navbar top -->
+		<div class="navbar-top">
+				<!-- site brand	 -->
+				<div class="site-brand">
+					<a href="index.html"><h1>Mstore</h1></a>
+				</div>
+				<!-- end site brand	 -->
+				<div class="side-nav-panel-right">
+					<a href="#" data-activates="slide-out-right" class="side-nav-left"><i class="fa fa-user"></i></a>
+					{{-- <img src="{{$tu ?? ''['headimgurl']}}" width="50"> --}}
+				</div>
+			</div>
+			<!-- end navbar top -->
+		
+			<!-- side nav right-->
+			<div class="side-nav-panel-right">
+				<ul id="slide-out-right" class="side-nav side-nav-panel collapsible">
+					<li class="profil">
+						<img src="{{$tu['headimgurl']}}" alt="">
+						<h2> {{$tu['nickname']}} </h2>
+					</li>
+					<li><a href="setting.html"><i class="fa fa-cog"></i>Settings</a></li>
+					<li><a href="about-us.html"><i class="fa fa-user"></i>About Us</a></li>
+					<li><a href="contact.html"><i class="fa fa-envelope-o"></i>Contact Us</a></li>
+					<li><a href="login.html"><i class="fa fa-sign-in"></i>Login</a></li>
+					<li><a href="register.html"><i class="fa fa-user-plus"></i>Register</a></li>
+				</ul>
+			</div>
+			<!-- end side nav right-->  
     @yield('content')
     
 
