e9f26738bf28e30416de54994851b583607f8bc1
zsj-zsj <2211045409@qq.com>
2019-12-23 21:37:32 +0800

    bug

--- resources/views/layouts/weixin.blade.php
+++ resources/views/layouts/weixin.blade.php
@@ -76,7 +76,7 @@
 			wx.updateAppMessageShareData({
 				title: '分享测试', // 分享标题
 				desc: '描述', // 分享描述
-				link: 'http://1905zhangshaojie.comcto.com/public/img/aaa.jpg', // 分享链接，该链接域名或路径必须与当前页面对应的公众号JS安全域名一致
+				link: 'http://1905zhangshaojie.comcto.com/', // 分享链接，该链接域名或路径必须与当前页面对应的公众号JS安全域名一致
 				imgUrl: 'http://1905zhangshaojie.comcto.com/public/img/aaa.jpg', // 分享图标
 				success: function () {
 					// 设置成功
@@ -87,7 +87,7 @@
 			wx.updateTimelineShareData({
 				title: '分享测试', // 分享标题
 				desc: '描述', // 分享描述
-				link: 'http://1905zhangshaojie.comcto.com/public/img/aaa.jpg', // 分享链接，该链接域名或路径必须与当前页面对应的公众号JS安全域名一致
+				link: 'http://1905zhangshaojie.comcto.com/', // 分享链接，该链接域名或路径必须与当前页面对应的公众号JS安全域名一致
 				imgUrl: 'http://1905zhangshaojie.comcto.com/public/img/aaa.jpg', // 分享图标
 				success: function () {
 					// 设置成功
