cc66765308d490338b82ce82d0cbb80415b2ba3f
zsj-zsj <2211045409@qq.com>
2019-12-24 16:49:03 +0800

    sd

--- app/Http/Controllers/Index/IndexController.php
+++ app/Http/Controllers/Index/IndexController.php
@@ -13,12 +13,12 @@ class IndexController extends Controller
 {
     public function  wxlogin(){
         $code=$_GET['code'];
-        //$data=$this->accesstoken($code);
-
-        $url = "https://api.weixin.qq.com/sns/oauth2/access_token?appid=".env('WX_APPID')."&secret=".env('WX_APPSECRET')."&code={$code}&grant_type=authorization_code";
-        $res_json = file_get_contents($url);    // 请求接口，获取json响应
-        $data = json_decode($res_json,true);
-        
+        dump($code);
+        $data=$this->accesstoken($code);
+        dump($data);
+        // $url = "https://api.weixin.qq.com/sns/oauth2/access_token?appid=".env('WX_APPID')."&secret=".env('WX_APPSECRET')."&code={$code}&grant_type=authorization_code";
+        // $res_json = file_get_contents($url);    // 请求接口，获取json响应
+        // $data = json_decode($res_json,true);
         $openid=$data['openid'];
 
         $tu=WxUserModel::where(['openid'=>$openid])->first();
@@ -36,7 +36,7 @@ class IndexController extends Controller
         session(['nickname'=>$tu['nickname']]);
 
         //返回到这个视图
-        return  redirect('/');
+        //return  redirect('/');
     }
 
 
@@ -64,7 +64,7 @@ class IndexController extends Controller
 
     //根据code获取accesstoken
     public function accesstoken($code){
-        $url='https://api.weixin.qq.com/sns/oauth2/access_token?appid=wx8bc80f5949fda528&secret=f4852897a0b441624d7c845c878f2548&code='.$code.'&grant_type=authorization_code';
+        $url = "https://api.weixin.qq.com/sns/oauth2/access_token?appid=".env('WX_APPID')."&secret=".env('WX_APPSECRET')."&code={$code}&grant_type=authorization_code";
         $data=file_get_contents($url);
         $json=json_decode($data,true);
         return $json;
