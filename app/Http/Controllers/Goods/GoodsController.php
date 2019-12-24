e5e6dd450ac98cdb34045a18dd63993462c9f7c6
zsj-zsj <2211045409@qq.com>
2019-12-24 08:21:54 +0800

    s

--- app/Http/Controllers/Goods/GoodsController.php
+++ app/Http/Controllers/Goods/GoodsController.php
@@ -12,7 +12,22 @@ class GoodsController extends Controller
 
     //商品详情页
     public function index($id){
+        //微信配置
+        $nonceStr = Str::random(8);
+        $signature = "";
+        $wx_config = [
+            'appId'     => env('WX_APPID'),
+            'timestamp' => time(),
+            'nonceStr'  => $nonceStr,
+            //'signature' => $signature,
+            'jsApiList' => ['updateAppMessageShareData']
+        ];
+        $ticket = WxUserModel::getJsapiTicket();
+        $url = $_SERVER['APP_URL'] . $_SERVER['REQUEST_URI'];;      //  当前url
+        $jsapi_signature = WxUserModel::jsapiSign($ticket,$url,$wx_config);
+        $wx_config['signature'] = $jsapi_signature;
+
         $index=WxGoodsModel::where('id','=',$id)->first();
-        return view('goods.detail',['index'=>$index]);
+        return view('goods.detail',['index'=>$index,'wx_config'=>$wx_config]);
     }
 }
